0d352e19aa15aecefbd8745b2f4e96b89d64ce43
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-16 00:28:52 +0100

    update: update DB with blogs and posts

--- src/db/db.ts
+++ src/db/db.ts
@@ -13,5 +13,19 @@ export const db:DBType = {
         availableResolutions : [
             "P144"
         ]
+    }],
+    blogs:[{
+        id: "string",
+        name: "string",
+        description: "string",
+        websiteUrl: "string"
+    }],
+    posts: [{
+        id: "string",
+        title: "string",
+        shortDescription: "string",
+        content: "string",
+        blogId: "string",
+        blogName: "string"
     }]
 }
