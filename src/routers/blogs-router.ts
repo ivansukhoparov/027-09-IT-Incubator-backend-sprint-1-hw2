f319154c86178056322a683222d84d88d03478dd
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-16 01:00:19 +0100

    create: add GET and GET by id for blogs

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -1,3 +1,13 @@
-import Router from "express";
+import {Router,Request, Response} from "express";
+import {BlogRepository} from "../repositories/blog-repository";
+import {Params, RequestWithParams} from "../types/common";
 
 export const blogsRouter = Router();
+
+blogsRouter.get("/", (req:Request,res:Response)=>{
+    BlogRepository.getAllBlogs();
+})
+
+blogsRouter.get("/:id", (req:RequestWithParams<Params>,res:Response)=>{
+    BlogRepository.getBlogById(req.params.id);
+})
