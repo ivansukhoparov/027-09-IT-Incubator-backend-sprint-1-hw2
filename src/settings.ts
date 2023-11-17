174a46e9517455c6e7376fd692741fe033df307b
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-18 00:29:00 +0100

    add bodyParser middleware

--- src/settings.ts
+++ src/settings.ts
@@ -3,11 +3,12 @@ import {videosRouter} from "./routers/videos-router";
 import {testingRouter} from "./routers/testing-router";
 import {blogsRouter} from "./routers/blogs-router";
 import {postsRouter} from "./routers/posts-router";
+import bodyParser from "body-parser";
 
 export const app = express();
 
 app.use(express.json());
-
+app.use(bodyParser());
 app.use("/testing", testingRouter);
 
 app.use("/videos", videosRouter);
