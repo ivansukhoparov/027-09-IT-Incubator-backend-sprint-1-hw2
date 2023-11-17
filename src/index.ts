e7a9e7badf442669e3078236833f4a24c1db3192
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-18 00:25:18 +0100

    add link to website to console when started

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const port:80 = 80;
 import {app} from "./settings";
 
 app.listen(port, ():void=>{
-    console.log(`listening port: ${port}`);
+    console.log(`app start on port ${port}`);
+    console.log(`open in browser http://localhost:${port}`);
 })
 
