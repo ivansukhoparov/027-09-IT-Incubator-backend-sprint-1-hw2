ac1b0f92f0417f202fbd9fa9207e031a934fe166
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-18 00:26:45 +0100

    create routers for blogs

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -1,13 +1,52 @@
-import {Router,Request, Response} from "express";
+import {Router, Request, Response} from "express";
 import {BlogRepository} from "../repositories/blog-repository";
-import {Params, RequestWithParams} from "../types/common";
+import {HTTP_STATUSES, Params, RequestWithBody, RequestWithBodyAndParams, RequestWithParams} from "../types/common";
+import {CreateBlogDto, UpdateBlogDto} from "../types/blogs/input";
+
 
 export const blogsRouter = Router();
 
-blogsRouter.get("/", (req:Request,res:Response)=>{
-    BlogRepository.getAllBlogs();
+blogsRouter.get("/", (req: Request, res: Response) => {
+    const blogs = BlogRepository.getAllBlogs();
+    res.status(HTTP_STATUSES.OK_200).json(blogs);
 })
 
-blogsRouter.get("/:id", (req:RequestWithParams<Params>,res:Response)=>{
-    BlogRepository.getBlogById(req.params.id);
+blogsRouter.get("/:id", (req: RequestWithParams<Params>, res: Response) => {
+    const blog = BlogRepository.getBlogById(req.params.id);
+    if (blog) {
+        res.status(HTTP_STATUSES.OK_200).json(blog);
+        return
+    } else {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+    }
+})
+
+blogsRouter.post('/', (req: RequestWithBody<CreateBlogDto>, res: Response) => {
+    const creatData = req.body;
+    const blogID = BlogRepository.createBlog(creatData);
+    const newBlog = BlogRepository.getBlogById(blogID);
+    if (newBlog) {
+        res.status(HTTP_STATUSES.CREATED_201).json(newBlog);
+        return
+    }
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+})
+
+blogsRouter.put("/:id", (req:RequestWithBodyAndParams<Params, UpdateBlogDto>, res:Response)=>{
+    const updateData = req.body;
+    const isUpdated = BlogRepository.updateBlog(req.params.id, updateData);
+    if (isUpdated) {
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+        return
+    }
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+})
+
+blogsRouter.delete("/:id", (req:RequestWithParams<Params>, res:Response)=>{
+    const isDeleted = BlogRepository.deleteBlog(req.params.id);
+    if (isDeleted){
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    return;
+    }
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
 })
