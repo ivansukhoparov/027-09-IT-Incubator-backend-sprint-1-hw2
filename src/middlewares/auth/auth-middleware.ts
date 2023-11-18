902e3bd38bfa28f4cf46569dd81aacf6d60510cf
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-19 00:55:36 +0100

    fix: na error with authorization has been fixed

--- src/middlewares/auth/auth-middleware.ts
+++ src/middlewares/auth/auth-middleware.ts
@@ -4,19 +4,24 @@ import {HTTP_STATUSES} from "../../utils/comon";
 
 const login = "admin";
 const password = "qwerty";
-
+const method = "Basic"
 // middleware для проверки авторизации
 export const basicAuthorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
-    const authInput = req.header("authorization")?.split(" ")[1]; // получаем значение Basic из заголовка
+    const authHeader = req.header("authorization")?.split(" "); // Получаем значение поля в заголовке
     const auth = btoa(`${login}:${password}`); // кодируем наши логин и пароль в basic64
 
-    // сравниваем нашу пару логин:пароль закодированную в basic64 с парой, пришедшей в заголовке реквеста
-    if (authInput !== auth) {
-        // если пары не равны отправляем статус 401
+    if (authHeader) {
+        const authMethod = authHeader[0];
+        const authInput = authHeader[1] // получаем значение Basic из заголовка}
+
+        // сравниваем нашу пару логин:пароль закодированную в basic64 с парой, пришедшей в заголовке реквеста
+        if (authInput === auth || authMethod === method) {
+            // если пары равны переходим по цепочке дальше
+            next();
+        }
+    } else {
+        // в противном случае отправляем ошибку 401
         res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
-    } else{
-        // в противном случае переходим по цепочке дальше
-        next();
     }
 }
