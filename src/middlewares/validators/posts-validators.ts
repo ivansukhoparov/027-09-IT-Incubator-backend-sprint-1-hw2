36356e375ba7ff5da2bedef13724c0f729660219
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-19 02:26:18 +0100

    fix: fixed error with posts validations

--- src/middlewares/validators/posts-validators.ts
+++ src/middlewares/validators/posts-validators.ts
@@ -3,7 +3,7 @@ import {BlogsRepository} from "../../repositories/blogs-repository";
 
 
 const validatePostTitle = body("title").trim().isString().notEmpty().isLength({min: 1, max: 30})
-const validatePostDescription = body("shortDescription").trim().isString().isLength({min: 0, max: 100});
+const validatePostDescription = body("shortDescription").trim().isString().notEmpty().isLength({min: 0, max: 100});
 const validatePostContent = body("content").isString().isLength({min: 0, max: 1000});
 const validateBlogID = body("blogId").isString().notEmpty().custom(value => BlogsRepository.getBlogById(value)?.id === value);
 
